ec9108226f60442c2336d0bb1f63aa4bc8ef1eb7
fluffy29 <hmajzoub12@gmail.com>
2024-11-17 01:15:57 +0100

    Add SQLite database integration and update .gitignore

--- database.js
+++ database.js
@@ -1,27 +1,3 @@
-const express = require("express");
-const app = express();
-
-const port = 3000;
-const usersRouter = require("./routes/users.js");
-
-// MIDDLEWARE
-
-app.use(express.json());
-// users endpoint
-
-app.use("/api/", usersRouter);
-
-// HOME GET METHOD
-app.get("/", (req, res) => {
-  res.json({
-    msg: "welcome to my users API !",
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
 const sqlite3 = require("sqlite3").verbose();
 
 // Open the database connection
